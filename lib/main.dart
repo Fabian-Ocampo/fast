cac5fd94fdb366fa2e9af2b464e6209e61a4c8a2
FABIAN OCAMPO <72311373+Fabian-Ocampo@users.noreply.github.com>
2020-11-06 16:39:43 -0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
+import 'package:ejercicio0/providers/userPrv.dart';
+import 'package:ejercicio0/providers/variablesPrv.dart';
+import 'package:ejercicio0/screens/login.dart';
+import 'package:ejercicio0/screens/start.dart';
+import 'package:ejercicio0/screens/welcome.dart';
 import 'package:flutter/material.dart';
-
-import 'package:prueba/Screens/splash.dart';
-import 'package:prueba/Screens/inicio.dart';
-import 'package:prueba/Screens/cambio.dart';
-import 'package:prueba/Screens/login2.dart';
-import 'package:prueba/Screens/Datos.dart';
-import 'package:prueba/Screens/login.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserPrv()),
+        ChangeNotifierProvider(create: (_) => VariablesPrv()),
+      ],
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //debugShowCheckedModeBanner: false,
+      title: "Fast food App",
+      //home: Welcome(),
       home: Scaffold(
-          body: PageView(
-        children: <Widget>[
-          
-        Splash(),
-         Inicio(),
-         Cambio(),
-         Login(),
-         Login2(),
-         Datos(),
-         
-        ],
-      )),
+          body: PageView(physics: BouncingScrollPhysics(), children: <Widget>[
+        Start(),
+        Welcome(),
+      ])),
     );
   }
 }
